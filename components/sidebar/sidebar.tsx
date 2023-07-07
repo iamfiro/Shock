06453ef507860c13f01ec7bc0528af101eb50b2f
FIRO <72495729+anojds@users.noreply.github.com>
2023-07-08 00:50:44 +0900

    `next/image` delete prefetch function

--- components/sidebar/sidebar.tsx
+++ components/sidebar/sidebar.tsx
@@ -12,20 +12,20 @@ const SideBar = ({route}: IProps) => {
             <Container>
                 <ServiceIconNoText width={25} height={25} />
                 <ListWrap>
-                    <ListItem href={{ pathname: '/@me'}} prefetch={true} selected={route === 'index'} selectedColor="#EF7443">
+                    <ListItem href={{ pathname: '/@me'}} selected={route === 'index'} selectedColor="#EF7443">
                         <Image src={`/icon/sidebar/document${route === 'index' ? '-white' : ''}.png`} width={24} height={24} alt="home" />
                     </ListItem>
-                    <ListItem href={{ pathname: '/@me/category'}} prefetch={true} selected={route === 'category'} selectedColor="#e33ee9">
+                    <ListItem href={{ pathname: '/@me/category'}} selected={route === 'category'} selectedColor="#e33ee9">
                         <Image src={`/icon/sidebar/category${route === 'category' ? '-white' : ''}.png`} width={24} height={24} alt="home" />
                     </ListItem>
                     <Divider/>
-                    <ListItem href={{ pathname: '/@me/pomodoro'}} prefetch={true} selected={route === 'pomodoro'} selectedColor="#ef4343">
+                    <ListItem href={{ pathname: '/@me/pomodoro'}} selected={route === 'pomodoro'} selectedColor="#ef4343">
                         <Image src={`/icon/sidebar/alarm${route === 'pomodoro' ? '-white' : ''}.png`} width={24} height={24} alt="home" />
                     </ListItem>
-                    <ListItem href={{ pathname: '/@me/sound'}} prefetch={true} selected={route === 'sound'} selectedColor="#4377ef">
+                    <ListItem href={{ pathname: '/@me/sound'}} selected={route === 'sound'} selectedColor="#4377ef">
                         <Image src={`/icon/sidebar/sound${route === 'sound' ? '-white' : ''}.png`} width={24} height={24} alt="home" />
                     </ListItem>
-                    <ListItem href={{ pathname: '/@me/bot'}} prefetch={true} selected={route === 'bot'} selectedColor="#2fcd0c">
+                    <ListItem href={{ pathname: '/@me/bot'}} selected={route === 'bot'} selectedColor="#2fcd0c">
                         <Image src={`/icon/sidebar/bot${route === 'bot' ? '-white' : ''}.png`} width={24} height={24} alt="home" />
                     </ListItem>
                 </ListWrap>

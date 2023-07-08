f54d589e29a838c1d46794bf63713e4fac22fef4
baeksikoo <baeksikoo3622@gmail.com>
2023-07-09 00:39:38 +0900

    feature: 사이드바 이미지 로딩 지연 이슈 해결
    사이드바 이미지 로딩이 느려 사용자 경험(UX)이 저하되는 문제점이 있었습니다.
    이에따라 Image 태그에 Priority Prop을 true로 하여 이미지 로딩이 빨라지는지 확인할 예정입니다.

--- components/sidebar/sidebar.tsx
+++ components/sidebar/sidebar.tsx
@@ -5,33 +5,91 @@ import { ServiceIconNoText } from "../ServiceIcon";
 import { IProps } from "./sidebar.type";
 import { useRouter } from "next/router";
 
-const SideBar = ({route}: IProps) => {
-    const routes = useRouter()
-    return (
-        <>
-            <Container>
-                <ServiceIconNoText width={25} height={25} />
-                <ListWrap>
-                    <ListItem href={{ pathname: '/@me'}} selected={route === 'index'} selectedColor="#EF7443">
-                        <Image src={`/icon/sidebar/document${route === 'index' ? '-white' : ''}.png`} width={24} height={24} alt="home" />
-                    </ListItem>
-                    <ListItem href={{ pathname: '/@me/category'}} selected={route === 'category'} selectedColor="#e33ee9">
-                        <Image src={`/icon/sidebar/category${route === 'category' ? '-white' : ''}.png`} width={24} height={24} alt="home" />
-                    </ListItem>
-                    <Divider/>
-                    <ListItem href={{ pathname: '/@me/pomodoro'}} selected={route === 'pomodoro'} selectedColor="#ef4343">
-                        <Image src={`/icon/sidebar/alarm${route === 'pomodoro' ? '-white' : ''}.png`} width={24} height={24} alt="home" />
-                    </ListItem>
-                    <ListItem href={{ pathname: '/@me/sound'}} selected={route === 'sound'} selectedColor="#4377ef">
-                        <Image src={`/icon/sidebar/sound${route === 'sound' ? '-white' : ''}.png`} width={24} height={24} alt="home" />
-                    </ListItem>
-                    <ListItem href={{ pathname: '/@me/bot'}} selected={route === 'bot'} selectedColor="#2fcd0c">
-                        <Image src={`/icon/sidebar/bot${route === 'bot' ? '-white' : ''}.png`} width={24} height={24} alt="home" />
-                    </ListItem>
-                </ListWrap>
-            </Container>
-        </>
-    );
-}
+const SideBar = ({ route }: IProps) => {
+  const routes = useRouter();
+  return (
+    <>
+      <Container>
+        <ServiceIconNoText width={25} height={25} />
+        <ListWrap>
+          <ListItem
+            href={{ pathname: "/@me" }}
+            selected={route === "index"}
+            selectedColor="#EF7443"
+          >
+            <Image
+              priority
+              src={`/icon/sidebar/document${
+                route === "index" ? "-white" : ""
+              }.png`}
+              width={24}
+              height={24}
+              alt="home"
+            />
+          </ListItem>
+          <ListItem
+            href={{ pathname: "/@me/category" }}
+            selected={route === "category"}
+            selectedColor="#e33ee9"
+          >
+            <Image
+              priority
+              src={`/icon/sidebar/category${
+                route === "category" ? "-white" : ""
+              }.png`}
+              width={24}
+              height={24}
+              alt="home"
+            />
+          </ListItem>
+          <Divider />
+          <ListItem
+            href={{ pathname: "/@me/pomodoro" }}
+            selected={route === "pomodoro"}
+            selectedColor="#ef4343"
+          >
+            <Image
+              priority
+              src={`/icon/sidebar/alarm${
+                route === "pomodoro" ? "-white" : ""
+              }.png`}
+              width={24}
+              height={24}
+              alt="home"
+            />
+          </ListItem>
+          <ListItem
+            href={{ pathname: "/@me/sound" }}
+            selected={route === "sound"}
+            selectedColor="#4377ef"
+          >
+            <Image
+              priority
+              src={`/icon/sidebar/sound${
+                route === "sound" ? "-white" : ""
+              }.png`}
+              width={24}
+              height={24}
+              alt="home"
+            />
+          </ListItem>
+          <ListItem
+            href={{ pathname: "/@me/bot" }}
+            selected={route === "bot"}
+            selectedColor="#2fcd0c"
+          >
+            <Image
+              priority
+              src={`/icon/sidebar/bot${route === "bot" ? "-white" : ""}.png`}
+              width={24}
+              height={24}
+              alt="home"
+            />
+          </ListItem>
+        </ListWrap>
+      </Container>
+    </>
+  );
+};
 
-export default SideBar
+export default SideBar;
